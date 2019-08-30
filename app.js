26af57f03e3444cb8a301eb31a0a88c5ace5796b
Johnny Valles <vallesja@uci.edu>
2019-08-30 10:58:00 -0700

    Create functions for API response processing and displaying

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 "use strict"
+
+function makeRepoHTML(data) {
+    for (let repo of data) {
+        let div1 = document.createElement("div");
+        let h2 = document.createElement("h2");
+        let div2 = document.createElement("div");
+        let ul = document.createElement("ul");
+        let span1 = document.createElement("span");
+        let span2 = document.createElement("span");
+        let owner = document.createElement("li");
+        owner.textContent = `Owner: ${repo["owner"]["login"]}`;
+        ul.appendChild(owner);
+        span1.textContent = "42";
+        span2.textContent = "commits";
+        div2.appendChild(span1);
+        div2.appendChild(span2);
+        div1.className = "repo";
+        div1.dataset.repoName = repo["name"];
+        div1.dataset.ownerName = repo["owner"]["login"];
+        h2.textContent = repo["name"];
+        div2.className = "commit-count";
+        div1.appendChild(h2);
+        div1.appendChild(div2);
+        div1.appendChild(ul);
+        document.querySelector(".repo-view").appendChild(div1);
+    }
+}
+
+function showRepo(event, data) {
+    makeRepoHTML(JSON.parse(this.responseText));
+}
+
+function getRepo() {
+    const req = new XMLHttpRequest();
+    req.open("GET", "https://api.github.com/users/octocat/repos");
+    req.send();
+    req.addEventListener("load", showRepo);
+}
